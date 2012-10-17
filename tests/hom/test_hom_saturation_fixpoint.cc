b880aa58a8b53bcad1bc13ed1e58ae684aa7f35d
Alexandre Hamez <alexandre.hamez@gmail.com>
2012-10-17 10:35:31 +0200

    Compile tests.

--- tests/hom/test_hom_saturation_fixpoint.cc
+++ tests/hom/test_hom_saturation_fixpoint.cc
@@ -13,6 +13,9 @@ const hom id = sdd::hom::Id<conf>();
 struct hom_saturation_fixpoint_test
   : public testing::Test
 {
+  hom_saturation_fixpoint_test()
+  {
+  }
 };
 
 /*-------------------------------------------------------------------------------------------*/
@@ -20,13 +23,13 @@ struct hom_saturation_fixpoint_test
 TEST_F(hom_saturation_fixpoint_test, construction)
 {
   {
-    std::vector<hom> g {id, Inductive<conf>(targeted_noop(0))};
+    std::vector<hom> g {id, Inductive<conf>(targeted_noop("0"))};
     ASSERT_EQ( SaturationFixpoint<conf>(0, id, g.begin(), g.end(), id)
              , SaturationFixpoint<conf>(0, id, g.begin(), g.end(), id));
   }
   {
-    std::vector<hom> g1 {id, Inductive<conf>(targeted_noop(0))};
-    std::vector<hom> g2 {id, Inductive<conf>(targeted_noop(2))};
+    std::vector<hom> g1 {id, Inductive<conf>(targeted_noop("0"))};
+    std::vector<hom> g2 {id, Inductive<conf>(targeted_noop("2"))};
     ASSERT_NE( SaturationFixpoint<conf>(0, id, g1.begin(), g1.end(), id)
              , SaturationFixpoint<conf>(0, id, g2.begin(), g2.end(), id));
   }
@@ -37,31 +40,34 @@ TEST_F(hom_saturation_fixpoint_test, construction)
 TEST_F(hom_saturation_fixpoint_test, evaluation)
 {
   {
-    SDD s0('a', {0}, SDD('b', {0}, SDD('c', {0}, one)));
+    const order<conf> o {"a", "b", "c"};
+    SDD s0(2, {0}, SDD(1, {0}, SDD(0, {0}, one)));
 
-    const hom f = Fixpoint(Sum<conf>({Inductive<conf>(targeted_incr('c', 1)), id}));
-    const std::vector<hom> g{Inductive<conf>(targeted_incr('b', 2))};
-    const hom h = SaturationFixpoint('b', f, g.begin(), g.end(), id);
+    const hom f = Fixpoint(Sum<conf>(o, {Inductive<conf>(targeted_incr("c", 1)), id}));
+    const std::vector<hom> g{Inductive<conf>(targeted_incr("b", 2))};
+    const hom h = SaturationFixpoint(1, f, g.begin(), g.end(), id);
 
-    const hom r = Fixpoint(Sum<conf>({ Inductive<conf>(targeted_incr('c', 1))
-                                     , Inductive<conf>(targeted_incr('b', 2))
-                                     , id}));
-    ASSERT_EQ(r(s0), h(s0));
-    ASSERT_EQ( SDD('a', {0}, SDD('b', {0,2}, SDD('c', {0,1,2}, one)))
-             , h(s0));
+    const hom r = Fixpoint(Sum<conf>(o, { Inductive<conf>(targeted_incr("c", 1))
+                                        , Inductive<conf>(targeted_incr("b", 2))
+                                        , id}));
+    ASSERT_EQ(r(o, s0), h(o, s0));
+    ASSERT_EQ( SDD(2, {0}, SDD(1, {0,2}, SDD(0, {0,1,2}, one)))
+             , h(o, s0));
   }
   {
-    SDD s0('a', {0}, SDD('b', SDD('x', {0}, one), SDD('c', {0}, one)));
+    order<conf> o;
+    o.add("c").add("b", order<conf>({"x"})).add("a");
+    SDD s0(2, {0}, SDD(1, SDD(0, {0}, one), SDD(0, {0}, one)));
 
-    const hom f = Fixpoint(Sum<conf>({Inductive<conf>(targeted_incr('c', 1)), id}));
+    const hom f = Fixpoint(Sum<conf>(o, {Inductive<conf>(targeted_incr("c", 1)), id}));
     const std::vector<hom> g{};
-    const hom l = Local('b', Sum<conf>({Inductive<conf>(targeted_incr('x', 2)), id}));
-    const hom h = SaturationFixpoint('b', f, g.begin(), g.end(), l);
+    const hom l = Local("b", o, Sum<conf>(o, {Inductive<conf>(targeted_incr("x", 2)), id}));
+    const hom h = SaturationFixpoint(1, f, g.begin(), g.end(), l);
 
-    const hom r = Fixpoint(Sum<conf>({ Inductive<conf>(targeted_incr('c', 1))
-                                     , Local('b', Inductive<conf>(targeted_incr('x', 2)))
-                                     , id}));
-    ASSERT_EQ(r(s0), h(s0));
+    const hom r = Fixpoint(Sum<conf>(o, { Inductive<conf>(targeted_incr("c", 1))
+                                        , Local("b", o, Inductive<conf>(targeted_incr("x", 2)))
+                                        , id}));
+    ASSERT_EQ(r(o, s0), h(o, s0));
   }
 }
 
