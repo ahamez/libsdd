83c4d8b935b686c28be44e73d99c4976988e4d57
Alexandre Hamez <alexandre.hamez@gmail.com>
2012-11-12 18:54:12 +0100

    Add new default configuration.

--- sdd/conf/default_configurations.hh
+++ sdd/conf/default_configurations.hh
@@ -4,6 +4,7 @@
 #include <string>
 
 #include "sdd/values/bitset.hh"
+#include "sdd/values/unique_flat_set.hh"
 
 namespace sdd { namespace conf {
 
@@ -22,6 +23,15 @@ struct conf0
 {
   typedef unsigned char      Variable;
   typedef values::bitset<64> Values;
+};
+
+/*-------------------------------------------------------------------------------------------*/
+
+struct conf1
+  : public default_parameters
+{
+  typedef unsigned int Variable;
+  typedef values::unique_flat_set<unsigned int> Values;
 };
 
 /*-------------------------------------------------------------------------------------------*/
