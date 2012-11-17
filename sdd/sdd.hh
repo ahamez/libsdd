8b337c438da9f2f7d987bfc9304d99591e2792e5
Alexandre Hamez <alexandre.hamez@gmail.com>
2012-11-17 16:37:00 +0100

    Documentation.

--- sdd/sdd.hh
+++ sdd/sdd.hh
@@ -13,6 +13,9 @@
 namespace sdd
 {
 
+/// @brief Internal stuff related to the library configuration.
+namespace conf {}
+
 /// @brief Internal stuff for decision diagrams.
 namespace dd {}
 
@@ -27,6 +30,9 @@ namespace util {}
 
 /// @brief Definition of values to be stored on arcs.
 namespace values {}
+
+/// @brief Essentially hash specialization.
+namespace std {}
 
 }
 
