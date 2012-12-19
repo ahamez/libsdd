472a8cb007d1eef8352856b6885fd305ef3f3123
Alexandre Hamez <alexandre.hamez@gmail.com>
2012-12-19 12:13:07 +0100

    Consolidate was missing...

--- sdd/hom/consolidate.hh
+++ sdd/hom/consolidate.hh
@@ -1,14 +1,29 @@
-//
-//  consolidate.hh
-//  libsdd
-//
-//  Created by Alexandre Hamez on 18/12/12.
-//
-//
+#ifndef _SDD_HOM_CONSOLIDATE_HH_
+#define _SDD_HOM_CONSOLIDATE_HH_
 
-#ifndef libsdd_consolidate_hh
-#define libsdd_consolidate_hh
+#include <iterator> // iterator_traits
 
+namespace sdd { namespace hom {
 
+/*------------------------------------------------------------------------------------------------*/
 
-#endif
+/// @internal
+template <typename InputIterator>
+void
+consolidate(char* addr, InputIterator it, InputIterator end)
+noexcept
+{
+  typedef typename std::iterator_traits<InputIterator>::value_type value_type;
+  value_type* base = reinterpret_cast<value_type*>(addr);
+  std::size_t i = 0;
+  for (; it != end; ++it)
+  {
+    new (base + i++) value_type(std::move(*it));
+  }
+}
+
+/*------------------------------------------------------------------------------------------------*/
+
+}} // namespace sdd::hom
+
+#endif // _SDD_HOM_CONSOLIDATE_HH_
