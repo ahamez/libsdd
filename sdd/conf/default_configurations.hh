15f9020d379171a175a9c14c75fa7de3a4384b5b
Alexandre Hamez <alexandre.hamez@gmail.com>
2013-11-07 08:54:23 +0100

    Documentation.

--- sdd/conf/default_configurations.hh
+++ sdd/conf/default_configurations.hh
@@ -11,20 +11,42 @@ namespace sdd {
 
 /*------------------------------------------------------------------------------------------------*/
 
+/// @brief The default base configuration.
+///
+/// It doesn't include the configuration of variable, identifier and values type. These informations
+/// should be given by derived configurations.
 struct default_configuration
 {
+  /// @brief The type to store the number of elements in an alpha successor function.
   typedef std::uint16_t alpha_size_type;
+
+  /// @brief The type to store the number of elements in an operation.
   typedef std::uint32_t operands_size_type;
 
+  /// @brief The initial size of the hash table that stores SDD.
   std::size_t sdd_unique_table_size;
+
+  /// @brief The size of the cache of SDD difference operations.
   std::size_t sdd_difference_cache_size;
+
+  /// @brief The size of the cache of SDD intersection operations.
   std::size_t sdd_intersection_cache_size;
+
+  /// @brief The size of the cache of SDD sum (union) operations.
   std::size_t sdd_sum_cache_size;
+
+  /// @brief The initial size of the hash table that stores homomorphisms.
   std::size_t hom_unique_table_size;
+
+  /// @brief The size of the cache of homomorphism applications.
   std::size_t hom_cache_size;
 
+  /// @brief Tell if FPU registers shoud be preserved when using Expressions.
   static constexpr bool expression_preserve_fpu_registers = false;
 
+  /// @brief Default constructor.
+  ///
+  /// Initialize all parameters to their default values.
   default_configuration()
     : sdd_unique_table_size(1000000)
     , sdd_difference_cache_size(500000)
