3c9d5fff40873423d7ff7fca1d4967abfc4cb098
Alexandre Hamez <alexandre.hamez@gmail.com>
2014-03-01 08:49:43 +0100

    Refine the assertion made at the arena's end of life.
    The previous assertion didn't take into account that some memory could be allocated outside the scope of a rewinder.

--- sdd/mem/linear_alloc.hh
+++ sdd/mem/linear_alloc.hh
@@ -12,29 +12,57 @@ namespace sdd { namespace mem {
 namespace {
 
 /// @internal
+/// @brief A memory arena for linear_alloc.
+///
+/// Everything is private in this class: this arena can only be manipulated with a rewinder and
+/// a linear_alloc.
 struct arena
 {
+  // Can't copy an arena.
+  arena(const arena&) = delete;
+  arena& operator=(const arena&) = delete;
+
   using position_type = char*;
 
+  /// @brief The size of this memory arena.
   const std::size_t size_;
+
+  /// @brief The underlying memory buffer.
   std::unique_ptr<char> buffer_;
+
+  /// @brief The beginning of the free memory.
   position_type position_;
 
+#ifndef NDEBUG
+  /// @brief The number of time this arena has been used with a rewinder.
+  unsigned int active_;
+
+  /// @brief The number of allocated bytes when this arena wasn't referenced by any rewinder.
+  std::size_t unactive_allocated_;
+#endif
+
+  /// @brief Construct an arena with a given size.
   arena(std::size_t size)
   noexcept
     : size_(size)
     , buffer_(new char[size_])
     , position_(buffer_.get())
+#ifndef NDEBUG
+    , active_(0)
+    , unactive_allocated_(0)
+#endif
   {}
 
+#ifndef NDEBUG
   ~arena()
   {
-    assert(used() == 0 && "Memory arena not rewound.");
+    // We admit that some bytes can never be deallocated. As a matter of fact, it would be very
+    // complex to keep track of the memory allocated when no rewinder is used. For instance,
+    // when the user creates a sum, the underlying container uses the linear allocator, but without
+    // a rewinder. This is why we keep a trace of the number of rewinders with active_.
+    assert(used() == unactive_allocated_ && "Memory arena not rewound.");
   }
-
-  // Can't copy an arena.
-  arena(const arena&) = delete;
-  arena& operator=(const arena&) = delete;
+#endif
 
   char*
   allocate(std::size_t n)
@@ -42,11 +70,17 @@ struct arena
     assert(pointer_in_buffer(position_) && "linear_alloc has outlived arena");
     if (buffer_.get() + size_ - position_ >= n)
     {
+#ifndef NDEBUG
+      if (active_ == 0)
+      {
+        unactive_allocated_ += n;
+      }
+#endif
       char* r = position_;
       position_ += n;
       return r;
     }
-    // Not enough room in the buffer, fallback to default allocator.
+    // Not enough room in the buffer, fallback to the default allocator.
     return static_cast<char*>(::operator new(n));
   }
 
@@ -59,21 +93,20 @@ struct arena
     {
       if (p + n == position_) // The memory pointed by p was the last allocated one.
       {
+#ifndef NDEBUG
+        if (active_ == 0)
+        {
+          unactive_allocated_ -= n;
+        }
+#endif
         position_ = p;
       }
     }
     else
     {
-      // The memory pointer by p was no allocated in this arena.
+      // The memory pointed by p was no allocated in this arena.
       ::operator delete(p);
     }
-  }
-
-  std::size_t
-  used()
-  const noexcept
-  {
-    return static_cast<std::size_t>(position_ - buffer_.get());
   }
 
   void
@@ -97,6 +130,29 @@ struct arena
   {
     return buffer_.get() <= p and p <= buffer_.get() + size_;
   }
+
+#ifndef NDEBUG
+  std::size_t
+  used()
+  const noexcept
+  {
+    return static_cast<std::size_t>(position_ - buffer_.get());
+  }
+
+  void
+  activate()
+  noexcept
+  {
+    active_ += 1;
+  }
+
+  void
+  deactivate()
+  noexcept
+  {
+    active_ -= 1;
+  }
+#endif
 };
 
 /*------------------------------------------------------------------------------------------------*/
@@ -105,30 +161,8 @@ struct arena
 /// @brief Rewind an arena to its initial position.
 ///
 /// Use RAII to ensure that the arena is always rewound to its initial position .
-struct rewinder
+class rewinder
 {
-  /// @brief The associated memory arena.
-  arena& a_;
-
-  /// @brief The position where to rewind the associated arena.
-  const arena::position_type pos_;
-
-  /// @brief Constructor.
-  ///
-  /// Initialize the position to the current position of the arena.
-  rewinder(arena& a)
-  noexcept
-    : a_(a), pos_(a.position())
-  {}
-
-  /// @brief Destructor.
-  ///
-  /// Rewind the arena to its position when this object was constructed;
-  ~rewinder()
-  {
-    a_.rewind(pos_);
-  }
-
   // Can't copy a rewinder.
   rewinder(const rewinder&) = delete;
   rewinder& operator=(const rewinder&) = delete;
@@ -136,6 +170,39 @@ struct rewinder
   // Can't move a rewinder.
   rewinder(rewinder&&) = delete;
   rewinder& operator=(rewinder&&) = delete;
+
+private:
+
+  /// @brief The associated memory arena.
+  arena& a_;
+
+  /// @brief The position where to rewind the associated arena.
+  const arena::position_type pos_;
+
+public:
+
+  /// @brief Constructor.
+  ///
+  /// Initialize the position to the current position of the arena.
+  rewinder(arena& a)
+  noexcept
+    : a_(a), pos_(a.position())
+  {
+#ifndef NDEBUG
+    a_.activate();
+#endif
+  }
+
+  /// @brief Destructor.
+  ///
+  /// Rewind the arena to its position when this object was constructed;
+  ~rewinder()
+  {
+    a_.rewind(pos_);
+#ifndef NDEBUG
+    a_.deactivate();
+#endif
+  }
 };
 
 /*------------------------------------------------------------------------------------------------*/
@@ -151,14 +218,19 @@ struct rewinder
 /// one thas was allocated can be deallocated. Thus, this allocator would mostly benefit to
 /// recursive algorithms which need a stack. 
 /// It's an adaptation of http://home.roadrunner.com/~hinnant/stack_alloc.html .
-template <class T>
-struct linear_alloc
+template <typename T>
+class linear_alloc
 {
+public:
+
   using value_type = T;
 
+private:
   arena& a_;
 
-  template <class _Up>
+public:
+
+  template <typename _Up>
   struct rebind
   {
     using other = linear_alloc<_Up>;
@@ -169,7 +241,7 @@ struct linear_alloc
     : a_(a)
   {}
 
-  template <class U>
+  template <typename U>
   linear_alloc(const linear_alloc<U>& other)
   noexcept
     : a_(other.a_)
@@ -192,7 +264,7 @@ struct linear_alloc
     a_.deallocate(reinterpret_cast<char*>(p), n*sizeof(T));
   }
 
-  template <class U, class V>
+  template <typename U, typename V>
   friend
   bool
   operator==(const linear_alloc<U>& lhs, const linear_alloc<V>& rhs)
@@ -201,7 +273,7 @@ struct linear_alloc
     return &lhs.a_ == &rhs.a_;
   }
 
-  template <class U> friend struct linear_alloc;
+  template <typename U> friend class linear_alloc;
 };
 
 /*------------------------------------------------------------------------------------------------*/
