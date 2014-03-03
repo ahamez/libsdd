84abc54eb67d83bb6b0be45130be965c23e6cfa7
Alexandre Hamez <alexandre.hamez@gmail.com>
2014-03-03 14:18:58 +0100

    More tests for the interrupt exception.

--- tests/hom/test_hom_interrupt.cc
+++ tests/hom/test_hom_interrupt.cc
@@ -89,6 +89,68 @@ struct interrupt_incr
 
 /*------------------------------------------------------------------------------------------------*/
 
+template <typename C>
+struct interrupt_incr_fun
+{
+  using values_type = typename C::Values;
+
+  const unsigned int value_;
+
+  interrupt_incr_fun(unsigned int val)
+    : value_(val)
+  {}
+
+  template <typename T>
+  values_type
+  operator()(const T& val)
+  const
+  {
+    if (val.find(2) != val.end())
+    {
+      throw sdd::interrupt<sdd::SDD<C>>();
+    }
+    else
+    {
+      T new_val;
+      for (const auto& v : val)
+      {
+        new_val.insert(v + value_);
+      }
+      return new_val;
+    }
+  }
+
+  sdd::values::bitset<64>
+  operator()(const sdd::values::bitset<64>& val)
+  const
+  {
+    if (val.content().test(2))
+    {
+      throw sdd::interrupt<sdd::SDD<C>>();
+    }
+    else
+    {
+      return val << value_;
+    }
+  }
+
+  bool
+  operator==(const interrupt_incr_fun& other)
+  const noexcept
+  {
+    return value_ == other.value_;
+  }
+
+  friend
+  std::ostream&
+  operator<<(std::ostream& os, const interrupt_incr_fun& i)
+  {
+    return os << "interrupt_incr_fun(" << i.value_ << ")";
+  }
+};
+
+/*------------------------------------------------------------------------------------------------*/
+
 namespace std {
 
 template <typename C>
@@ -99,6 +161,17 @@ struct hash<interrupt_incr<C>>
   const noexcept
   {
     return std::hash<std::string>()(i.var_) xor std::hash<unsigned int>()(i.value_);
+  }
+};
+
+template <typename C>
+struct hash<interrupt_incr_fun<C>>
+{
+  std::size_t
+  operator()(const interrupt_incr_fun<C>& i)
+  const noexcept
+  {
+    return std::hash<unsigned int>()(i.value_);
   }
 };
 
@@ -133,7 +206,53 @@ TYPED_TEST_CASE(hom_interruption_test, configurations);
 
 /*------------------------------------------------------------------------------------------------*/
 
-TYPED_TEST(hom_interruption_test, fixpoint)
+TYPED_TEST(hom_interruption_test, function_fixpoint)
+{
+  {
+    order o(order_builder {"2", "1", "0"});
+    SDD s0(o, [](const std::string&){return values_type{0};});
+    homomorphism h0 = fixpoint(sum(o, {function(o, "0", interrupt_incr_fun<conf>(1)), id}));
+    ASSERT_THROW(h0(o, s0), sdd::interrupt<SDD>);
+    try
+    {
+      h0(o, s0);
+    }
+    catch (const sdd::interrupt<SDD>& i)
+    {
+      ASSERT_EQ(SDD(2, {0}, SDD(1, {0}, SDD(0, {0,1,2}, one))), i.result());
+    }
+  }
+  {
+    order o(order_builder {"2", "1", "0"});
+    SDD s0(o, [](const std::string&){return values_type{0};});
+    homomorphism h0 = fixpoint(sum(o, { function(o, "0", interrupt_incr_fun<conf>(1))
+                                      , function(o, "2", interrupt_incr_fun<conf>(1))
+                                      , id
+                                      }));
+    ASSERT_THROW(h0(o, s0), sdd::interrupt<SDD>);
+    try
+    {
+      h0(o, s0);
+    }
+    catch (const sdd::interrupt<SDD>& i)
+    {
+      // It's hard to test the result as the order of application of the sum operands is not
+      // determinist. Thus, we can't check for an exact result.
+      ASSERT_NE(zero, i.result());
+      // At least, we know that it can't be the same result as if interrupt_incr_fun didn't
+      // interrupt the evaluation.
+      homomorphism h1 = fixpoint(sum(o, { inductive<conf>(targeted_incr<conf>("0", 1))
+                                        , inductive<conf>(targeted_incr<conf>("2", 1))
+                                        , id
+                                        }));
+      ASSERT_NE(h1(o, s0), i.result());
+    }
+  }
+}
+
+/*------------------------------------------------------------------------------------------------*/
+
+TYPED_TEST(hom_interruption_test, inductive_fixpoint)
 {
   {
     order o(order_builder {"2", "1", "0"});
