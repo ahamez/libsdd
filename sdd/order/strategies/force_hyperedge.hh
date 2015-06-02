ba25604a24933a69b160e2b48e1d192f0d25ccf6
Alexandre Hamez <alexandre.hamez@gmail.com>
2015-06-02 11:06:29 +0200

    Copyright notice

--- sdd/order/strategies/force_hyperedge.hh
+++ sdd/order/strategies/force_hyperedge.hh
@@ -1,3 +1,8 @@
+/// @file
+/// @copyright The code is licensed under the BSD License
+///            <http://opensource.org/licenses/BSD-2-Clause>,
+///            Copyright (c) 2012-2015 Alexandre Hamez.
+/// @author Alexandre Hamez
 #pragma once
 
 #include <algorithm>  // minmax_element
