bfa68e247bb3b3bfb7fcc29b4b292c8f5f3fd0ac
Alexandre Hamez <alexandre.hamez@gmail.com>
2015-09-03 05:57:10 +0200

    Don't show warnings for external dependancies

--- sdd/dd/count_combinations_fwd.hh
+++ sdd/dd/count_combinations_fwd.hh
@@ -5,18 +5,7 @@
 /// @author Alexandre Hamez
 #pragma once
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-#pragma GCC diagnostic ignored "-Wshadow"
-#ifdef __clang__
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wshorten-64-to-32"
-#endif
 #include <boost/multiprecision/cpp_int.hpp>
-#ifdef __clang__
-#pragma clang diagnostic pop
-#endif
-#pragma GCC diagnostic pop
 
 #include "sdd/dd/definition.hh"
 
