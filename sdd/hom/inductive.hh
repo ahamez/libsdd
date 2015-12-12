0bbd2054d86248f0c1bb38b9e2315957a2365e77
Alexandre Hamez <alexandre.hamez+github@gmail.com>
2015-12-12 10:51:19 +0100

    Cleanup projects and ship with gtest

--- sdd/hom/inductive.hh
+++ sdd/hom/inductive.hh
@@ -3,6 +3,7 @@
 ///            <http://opensource.org/licenses/BSD-2-Clause>,
 ///            Copyright (c) 2012-2015 Alexandre Hamez.
 /// @author Alexandre Hamez
+
 #pragma once
 
 #include <cassert>
--- sdd/values/flat_set.hh
+++ sdd/values/flat_set.hh
@@ -3,6 +3,7 @@
 ///            <http://opensource.org/licenses/BSD-2-Clause>,
 ///            Copyright (c) 2012-2015 Alexandre Hamez.
 /// @author Alexandre Hamez
+
 #pragma once
 
 #include <algorithm>   // copy, set_difference, set_intersection, set_union
